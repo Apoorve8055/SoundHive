38dcb068f36e488e9906df33cf5e1b5ba05c8140
Apoorve Verma <vapoorve@gmail.com>
2023-04-11 11:30:23 +0530

    Story: Create the Error page - Completed!

--- client/src/pages/errors/ErrorPage.js
+++ client/src/pages/errors/ErrorPage.js
@@ -1,17 +1,30 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ErrorCode from "../../constant/ErrorCodes";
+import robotPng from "../../assets/png/robot.png";
 
+import "./ErrorPage.scss";
+import { IoArrowBack } from "react-icons/io5";
 const ErrorPage = () => {
   const codeParams = useParams();
+  const navigate = useNavigate();
   let code = 404;
   if (codeParams.code) code = codeParams.code;
 
   return (
-    <div>
-      <h1>Hi</h1>
-      <h1>{ErrorCode[code].name}</h1>
-      <h2>{ErrorCode[code].code}</h2>
-      <h5>{ErrorCode[code].msg}</h5>
+    <div className="error-container">
+      <div className="error-card">
+        <img src={robotPng} className="error-robot-img" />
+        <div className="error-details">
+          <div className="back" onClick={() => navigate(-1)}>
+            <IoArrowBack />
+            Back
+          </div>
+          <h1>
+            {ErrorCode[code].code} &nbsp; {ErrorCode[code].name}
+          </h1>
+          <h5>{ErrorCode[code].msg}</h5>
+        </div>
+      </div>
     </div>
   );
 };
